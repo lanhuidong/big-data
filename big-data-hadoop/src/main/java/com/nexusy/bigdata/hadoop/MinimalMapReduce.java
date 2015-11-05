777402bb9538694f70f1f88ff2672c72bb58bdca
Lan <lanhuidong@gmail.com>
2015-11-05 22:59:44 +0800

    添加一个最简单的MapReduce程序

---
+++ big-data-hadoop/src/main/java/com/nexusy/bigdata/hadoop/MinimalMapReduce.java
@@ -0,0 +1,49 @@
+package com.nexusy.bigdata.hadoop;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * @author lanhuidong
+ * @since 2015-11-05
+ *
+ * CMD: hadoop jar big-data-hadoop.jar com.nexusy.bigdata.hadoop.MinimalMapReduce nba.txt minimal
+ */
+public class MinimalMapReduce extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
+        if (job == null) {
+            return -1;
+        }
+
+        //下面10行代码是MapReduce的默认设置，即使注释掉运行结果也是一样的
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setMapperClass(Mapper.class);
+        job.setMapOutputKeyClass(LongWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setPartitionerClass(HashPartitioner.class);
+        job.setNumReduceTasks(1);
+        job.setReducerClass(Reducer.class);
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new MinimalMapReduce(), args);
+        System.exit(exitCode);
+    }
+}
